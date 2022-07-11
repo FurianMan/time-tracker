165897418a7577b075b99583b450d63fc476406f
vladislav.rikhter <vladislav.rikhter@eltex.loc>
2022-07-11 07:46:32 +0700

    begining

---
+++ docker/scripts/Script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS time_tracker;
+CREATE TABLE IF NOT EXISTS time_tracker.users(
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    patronymic VARCHAR(30),
+    position VARCHAR(30) NOT NULL,
+    birthday DATE NOT NULL
+);
+CREATE TABLE IF NOT EXISTS time_tracker.tasks(
+	task_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	number INT NOT NULL,
+	start_time DATETIME NOT NULL,
+	end_time DATETIME,
+	user_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES time_tracker.users (user_id) ON DELETE CASCADE
+);
